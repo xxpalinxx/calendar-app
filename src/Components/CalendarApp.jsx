1cc6f04f33a71e1728affa4d09f74a5e9b1eeed0
xxpalinxx <144467903+xxpalinxx@users.noreply.github.com>
2024-09-03 14:12:36 -0300

    actualizado calendario dinamico y marcacion de dia actual

--- src/Components/CalendarApp.jsx
+++ src/Components/CalendarApp.jsx
@@ -1,59 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CalendarApp = () => {
+    const daysOfWeek = ["Sun","Mon","Tue","Wen","Thu","Fri","Sat"]
+    const monthOfYear = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    const currentDate = new Date()
+    
+    const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth())
+    const [currentYear, setCurrentYear] = useState(currentDate.getFullYear())
+
+    const daysInMonth = new Date(currentYear, currentMonth + 1,  0).getDate()
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay()
+
+    const prevMonth = () => {
+        setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1))
+        setCurrentYear((prevYear => currentMonth === 0 ? prevYear - 1 : prevYear))
+    }
+
+    const nextMonth = () => {
+        setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1))
+        setCurrentYear((prevYear => currentMonth === 11 ? prevYear + 1 : prevYear))
+    }
+
     return (
         <div className='calendar-app'>
             <div className="calendar">
                 <h1 className="heading">Calendar</h1>
                 <div className="navigate-date">
-                    <h2 className="month">May,</h2>
-                    <h2 className="year">2024</h2>
+                    <h2 className="month">{monthOfYear[currentMonth]}</h2>
+                    <h2 className="year">{currentYear}</h2>
                     <div className="buttons">
-                        <i className="bx bx-chevron-left"></i>
-                        <i className="bx bx-chevron-right"></i>
+                        <i className="bx bx-chevron-left" onClick={prevMonth}></i>
+                        <i className="bx bx-chevron-right" onClick={nextMonth}></i>
                     </div>
                 </div>
                 <div className="weekdays">
-                    <span>Sun</span>
-                    <span>Mon</span>
-                    <span>Tue</span>
-                    <span>Wen</span>
-                    <span>Thu</span>
-                    <span>Fri</span>
-                    <span>Sat</span>
+                    {daysOfWeek.map((day) => (<span key={day}>{day}</span>))}
                 </div>
                 <div className="days">
-                    <span>1</span>
-                    <span>2</span>
-                    <span>3</span>
-                    <span>4</span>
-                    <span>5</span>
-                    <span>6</span>
-                    <span>7</span>
-                    <span>8</span>
-                    <span>9</span>
-                    <span>10</span>
-                    <span>11</span>
-                    <span>12</span>
-                    <span>13</span>
-                    <span>14</span>
-                    <span>15</span>
-                    <span>16</span>
-                    <span className='current-day'>17</span>
-                    <span>18</span>
-                    <span>19</span>
-                    <span>20</span>
-                    <span>21</span>
-                    <span>22</span>
-                    <span>23</span>
-                    <span>24</span>
-                    <span>25</span>
-                    <span>26</span>
-                    <span>27</span>
-                    <span>28</span>
-                    <span>29</span>
-                    <span>30</span>
-                    <span>31</span>
+                    {[...Array(firstDayOfMonth).keys()].map((_, index) => (<span key={`empty-${index}`}/>))}
+                    {[...Array(daysInMonth).keys()].map((day) => (<span key={day + 1} className={day + 1 === currentDate.getDate() && currentMonth === currentDate.getMonth() && currentYear === currentDate.getFullYear() ? 'current-day' : ""}>{day + 1}</span>))}
                 </div>
             </div>
             <div className="events">
